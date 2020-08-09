5a83d5ea63a91073669150aba29506d0345d7c9c
Subhalaxmi-21 <66870287+Subhalaxmi-21@users.noreply.github.com>
2020-08-09 22:36:44 +0530

    Update script.js

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -246,6 +246,7 @@ function buildAndShowMenuItemsHTML (categoryMenuItems) {
 function buildMenuItemsViewHtml(categoryMenuItems, 
                                 menuItemsTitleHtml,
                                 menuItemHtml) {
+  console.log(categoryMenuItems.category.name);
   
   menuItemsTitleHtml =
     insertProperty(menuItemsTitleHtml,
